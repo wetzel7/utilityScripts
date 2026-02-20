fc85deb446e0569841e205314db09688a920eb80
wetzel7 <loganpsavage15@gmail.com>
2026-02-19 19:40:27 -0500

    Refactor VM preparation script for user input
    Updated user input handling to read from prompt instead of command line arguments. Improved netplan file selection method.

--- bash/vmPrepareScript.sh
+++ bash/vmPrepareScript.sh
@@ -6,20 +6,21 @@
 # Variables required: USERNAME, HOSTNAME
 
 
-# Variables provided check
-if [ -z "$1" ] || [ -z "$2" ]; then
-	echo "Usage: $0 <username> <hostname>"
-fi
+# Get variables from user
+read -r -p "Enter a username and hostname for the VM seperated by spaces: " USERNAME HOSTNAME
 
-USERNAME=$1
-HOSTNAME=$2
+# Check user input
+if [ -z "$USERNAME" ] || [ -z "$HOSTNAME" ]; then
+    echo "Usage: please provide a username and hostname"
+    exit 1
+fi
 
 # user creation
 if id "$USERNAME" &>/dev/null; then
     echo "User $USERNAME exists."
 else
     sudo useradd -m -d /home/"$USERNAME" -s /bin/bash "$USERNAME"
-	sudo passwd $USERNAME
+	sudo passwd "$USERNAME"
 fi
 
 # user elevation dependant on OS
@@ -40,7 +41,7 @@ if grep -qi "ubuntu\|debian" /etc/os-release; then
 	mkdir -p "$BACKUP_DIR"
 	cp /etc/netplan/*.yaml "$BACKUP_DIR/"
 	# modify netplan file
-	NETPLAN_FILE=$(ls /etc/netplan/*.yaml | head -1)
+	NETPLAN_FILE=$(find /etc/netplan -maxdepth 1 -name "*.yaml" | head -1)
 
 	while true; do
 		sudo vi "$NETPLAN_FILE"
